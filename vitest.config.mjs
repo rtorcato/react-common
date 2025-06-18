0e032527edf4cd74eeadeee0759eb0ec2cc4a9c2
Richard Torcato <rtorcato@me.com>
2025-06-18 16:57:38 -0400

    chore: added files

---
+++ vitest.config.mjs
@@ -0,0 +1,24 @@
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
+
+export default defineConfig({
+	plugins: [react()],
+	test: {
+		globals: true,
+		environment: 'jsdom',
+		coverage: {
+			provider: 'istanbul',
+			reporter: ['text', 'json', 'html'],
+		},
+	},
+	resolve: {
+		alias: {
+			'@': path.resolve(__dirname, './src'),
+			'~': path.resolve(__dirname, './src'),
+		},
+	},
+})
