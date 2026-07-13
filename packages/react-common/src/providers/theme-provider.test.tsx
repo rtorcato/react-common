05cf8cdfde13dd3e1555ec489fa757af5aaa497f
Richard Torcato <richardtorcato@example.com>
2026-07-13 14:39:05 -0400

    refactor: organize react-common src into hooks/ + providers/ (closes #97)

--- packages/react-common/src/providers/theme-provider.test.tsx
+++ packages/react-common/src/providers/theme-provider.test.tsx
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react'
 import { describe, expect, it } from 'vitest'
 
-import { ThemeProvider, useTheme } from '~/theme-provider'
+import { ThemeProvider, useTheme } from '~/providers/theme-provider'
 
 function ThemeProbe() {
 	const { theme, themes } = useTheme()
